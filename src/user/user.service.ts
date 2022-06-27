b10431986b005856a89f98226de8e6d01cd3554b
Abanoub321 <abanoubmilad12@gmail.com>
2022-06-27 13:09:30 +0200

    Feat: Post: Register

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/createUser.dto';
+import { User } from './interfaces/user.interface';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+    users: User[] = [];
+   
+    createUser(user: CreateUserDto) {
+        this.users.push(user);
+        return user;
+    }
+}
