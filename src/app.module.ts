37e8bd037f2c7f69e814aec66dd7bfdbd90135c8
Abanoub321 <abanoubmilad12@gmail.com>
2022-06-30 15:53:56 +0200

    Initialize movie Route

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { DatabaseModule } from './database/database.module';
+import { MovieModule } from './movie/movie.module';
 
 @Module({
-  imports: [UserModule, DatabaseModule],
+  imports: [UserModule, DatabaseModule, MovieModule],
   controllers: [AppController],
   providers: [AppService],
 })
