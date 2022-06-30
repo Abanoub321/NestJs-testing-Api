cd7afc7e2cfa297fc76e0377f51a3bcce87c1d71
Abanoub321 <abanoubmilad12@gmail.com>
2022-06-30 17:02:42 +0200

    Feat: Creating movie

--- src/movie/movie.controller.ts
+++ src/movie/movie.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, UsePipes, ValidationPipe } from '@nestjs/common';
+import { NewMovieDto } from './dto/newMovie.dto';
+import { MovieService } from './movie.service';
 
 @Controller('movie')
-export class MovieController {}
+export class MovieController {
+    constructor(private readonly movieService: MovieService) { }
+
+    @Post('create')
+    @UsePipes(ValidationPipe)
+    async createMovie(@Body() newMovieDto: NewMovieDto) {
+        return await this.movieService.AddNewMovie(newMovieDto);
+    }
+
+}
--- src/movie/movie.module.ts
+++ src/movie/movie.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
+import { EntityProviders } from 'src/database/model.providers';
 import { MovieController } from './movie.controller';
 import { MovieService } from './movie.service';
 
 @Module({
   controllers: [MovieController],
-  providers: [MovieService]
+  providers: [MovieService, ...EntityProviders]
 })
 export class MovieModule {}
