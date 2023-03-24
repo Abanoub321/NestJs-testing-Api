43f9c522697cfd6c6d7b9b3c8b414d307a58c6cb
abanoub321 <abanoubmilad12@gmail.com>
2023-03-24 20:27:16 +0200

    Feat: using morgan for logs

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import * as morgan from 'morgan';
 import 'dotenv/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(morgan('dev'));
   const config = new DocumentBuilder()
     .setTitle('Movies API')
     .setDescription('The movies API description')
